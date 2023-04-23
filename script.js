b60070fe37f0696e2df5fca581c0949e5eb9440c
Mariam Ekizashvili <122484329+Mariawoland@users.noreply.github.com>
2023-04-23 23:41:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 function checkForBlank() {
 
-    if (document.getElementById('iMail').value == "") {
 
-        let setting_Mail = document.getElementById('iMail');
+    let setting_Mail = document.getElementById('iMail');
+    let redAlert = document.getElementById('pop-up-hide');
+
+    if (setting_Mail.value == "") {
+
         setting_Mail.style.borderColor = "rgb(139, 0, 0)";
         setting_Mail.style.background = "rgb(230, 130, 130)";
+        redAlert.style.display = "block";
         return false;
+
+    } else if (setting_Mail.value !== "") {
+
+        setting_Mail.style.borderColor = "transparent";
+        setting_Mail.style.background = "white"
+        redAlert.style.display = "none";
+
+        return;
     }
+
 }
